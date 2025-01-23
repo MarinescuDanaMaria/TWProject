82873d9865c04214482f8ad8ee4e4ddc9e5e8839
IacobVictoria <161374115+IacobVictoria@users.noreply.github.com>
2025-01-23 18:37:21 +0200

    Event is scheduled type of webinar.

--- EventsApp/src/components/User/WelcomeEventPage.jsx
+++ EventsApp/src/components/User/WelcomeEventPage.jsx
@@ -3,7 +3,7 @@ import { useParams } from "react-router-dom";
 
 function WelcomeEventPage() {
   const { id } = useParams();
-  const [event, setEvent] = useState(null); 
+  const [event, setEvent] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
@@ -47,27 +47,43 @@ function WelcomeEventPage() {
   if (!event) {
     return <div>Event not found.</div>;
   }
-
+  if (event.status === "SCHEDULED") {
+    return (
+      <div className="h-screen flex items-center justify-center bg-gray-100">
+        <div className="text-center max-w-3xl p-10 bg-white shadow-2xl rounded-lg">
+          <img
+            src="http://localhost:8081/images/events_web.png"
+            alt="Event Picture"
+            className="mx-auto mb-10 w-64 h-64 object-contain"
+          />
+          <h1 className="text-6xl font-extrabold mb-8 text-gray-800">
+            Event is scheduled
+          </h1>
+          <p className="text-2xl text-gray-600">Stay tuned for updates!</p>
+        </div>
+      </div>
+    );
+  }
   return (
-
     <div className="h-screen flex items-center justify-center bg-gray-100">
-    <div className="text-center max-w-3xl p-10 bg-white shadow-2xl rounded-lg">
-      <img
-        src="http://localhost:8081/images/events_web.png"
-        alt="Event Picture"
-        className="mx-auto mb-10 w-64 h-64 object-contain"
-      />
-      <h1 className="text-6xl font-extrabold mb-8 text-gray-800">WELCOME TO {event.name}</h1>
+      <div className="text-center max-w-3xl p-10 bg-white shadow-2xl rounded-lg">
+        <img
+          src="http://localhost:8081/images/events_web.png"
+          alt="Event Picture"
+          className="mx-auto mb-10 w-64 h-64 object-contain"
+        />
+        <h1 className="text-6xl font-extrabold mb-8 text-gray-800">
+          WELCOME TO {event.name}
+        </h1>
 
-      <p className="text-2xl mb-6 text-gray-600">
-        The event will end at:{" "}
-        <span className="font-bold text-gray-900">
-          {new Date(event.endTime).toLocaleString()}
-        </span>
-      </p>
+        <p className="text-2xl mb-6 text-gray-600">
+          The event will end at:{" "}
+          <span className="font-bold text-gray-900">
+            {new Date(event.endTime).toLocaleString()}
+          </span>
+        </p>
+      </div>
     </div>
-  </div>
-  
   );
 }
 
